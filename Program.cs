e576fe2bfcd61e82adc0f2de6fb109512b937cb2
Ikram Boublaou <102217342+boublaou@users.noreply.github.com>
2022-11-30 11:50:08 -0300

    Commit Inicial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Quadrado q1 = new Quadrado(5.0);
+
+Console.WriteLine(q1.Lado);
+
+q1.Lado = 2.0;
+
+Console.WriteLine(q1.Area);
+Console.WriteLine(q1.Perimetro);
+
+Retangulo r1 = new Retangulo(5.0, 5.0);
+
+Console.WriteLine(r1.Altura);
+
+r1.Altura = 2.0;
+
+Console.WriteLine(r1.Area);
+Console.WriteLine(r1.Perimetro);
